f92c28eb253999ddeab9ae7a5728494d0a9baa9f
Sri-hari-haran-g <102217519+Sri-hari-haran-g@users.noreply.github.com>
2023-08-09 17:45:06 +0530

    Add files via upload

---
+++ Vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PdfConversion from './components/PdfConversion.vue'
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <PdfConversion msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vue/src/components/PdfConversion.vue
@@ -0,0 +1,123 @@
+<script>
+export default{
+  data(){
+    return {
+      file :null
+    };
+  },
+  methods:{
+    
+    uploadFile(event){
+      debugger;
+      const formData = new FormData();
+      this.file = event.target.files[0];
+      formData.append('file', this.file);
+    },
+
+    async ConvertToPdf(){
+      debugger;
+      const formData = new FormData();
+      formData.append('file', this.file);
+      let response = await fetch("https://localhost:7019/api/Product/ConvertToPdf", {
+        method: 'POST',
+        body: formData
+      })
+      this.downloadFile(response, this.file);
+    },
+
+    async ConvertHtmlToPdf(){
+      debugger;
+      const formData = new FormData();
+      formData.append('file', this.file);
+      let response = await fetch("https://localhost:7019/api/Product/HtmlToPdf", {
+        method: 'POST',
+        body: formData
+      })
+      this.downloadFile(response, this.file);
+    },
+
+    async ConvertWordToPdf(){
+      debugger;
+      const formData = new FormData();
+      formData.append('file', this.file);
+      let response = await fetch("https://localhost:7019/api/Product/WordToPdf", {
+        method: 'POST',
+        body: formData
+      })
+      this.downloadFile(response, this.file);
+    },
+
+    async ConvertImageToPdf(){
+      debugger;
+      const formData = new FormData();
+      formData.append('file', this.file);
+      let response = await fetch("https://localhost:7019/api/Product/ImageToPdf", {
+        method: 'POST',
+        body: formData
+      })
+      this.downloadFile(response, this.file);
+    },
+
+    async ConvertPptxToPdf(){
+      debugger;
+      const formData = new FormData();
+      formData.append('file', this.file);
+      let response = await fetch("https://localhost:7019/api/Product/PptxToPdf", {
+        method: 'POST',
+        body: formData
+      })
+      this.downloadFile(response, this.file);
+      debugger;
+    },
+
+    async ConvertExcelToPdf(){
+      debugger;
+      const formData = new FormData();
+      formData.append('file', this.file);
+      let response = await fetch("https://localhost:7019/api/Product/ExcelToPdf", {
+        method: 'POST',
+        body: formData
+      })
+      this.downloadFile(response, this.file);
+      debugger;
+    },
+
+    async downloadFile(response, file) {
+    const fileName = file.name.split(".");
+    const fileSaveName = fileName[0];
+    const arrayBuffer = await response.arrayBuffer();
+    const byteArray = new Uint8Array(arrayBuffer);
+    const blob = new Blob([byteArray], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileSaveName+'.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  }
+};
+
+  
+ 
+
+</script>
+
+<template>
+  <div class="greetings">
+    <input type="file" @change="uploadFile" /><br><br>
+    <button @click="ConvertToPdf">Convert to PDF</button><br><br>
+    <button @click="ConvertHtmlToPdf">Convert HtML to PDF</button><br><br>
+    <button @click="ConvertWordToPdf">Convert Word to PDF</button><br><br>
+    <button @click="ConvertPptxToPdf">Convert Pptx to PDF</button><br><br>
+    <button @click="ConvertExcelToPdf">Convert Excel to PDF</button><br><br>
+    <button @click="ConvertImageToPdf">Convert Image to PDF</button><br><br>
+    
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
